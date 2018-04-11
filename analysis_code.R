3d567dbd7b7ef397872adf2cd70b87d1fd6475ed
Matiki <matiasbruzza@gmail.com>
2018-04-10 19:29:36 -0700

    added code to check file exiestence, download, unzip, and read data

--- analysis_code.R
+++ analysis_code.R
@@ -2,7 +2,18 @@ library(readr)
 library(dplyr)
 library(ggplot2)
 
-if(!file.exists("storm_data.csv")){
-        download.file("https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2FStormData.csv.bz2",
-                      destfile = "zipped_data.csv")
+if(!file.exists("zipped_storm_data.csv.bz2")){
+        URL <- "https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2FStormData.csv.bz2"
+        download.file(URL,
+                      destfile = "zipped_storm_data.csv.bz2")
 }
+
+if(!file.exists("storm_data.csv")){
+        library(R.utils)
+        bunzip2(filename = "zipped_storm_data.csv.bz2", 
+                destname = "storm_data.csv", 
+                remove = F, 
+                skip = T)
+        }
+
+storm_data <- read_csv("storm_data.csv")
