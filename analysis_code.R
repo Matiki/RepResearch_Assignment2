9972f0a4520ec70c2551d6cac0159b1f5c553ee6
Matiki <matiasbruzza@gmail.com>
2018-04-12 12:34:00 -0700

    Added: code to summarize data (mean/median/max/total) by fatalities and injuries

--- analysis_code.R
+++ analysis_code.R
@@ -34,8 +34,30 @@ storm_data <- storm_data %>%
 # Change EVTYPE to factor variable
 storm_data$EVTYPE <- as.factor(storm_data$EVTYPE)
 
-# Summarize data over mean/median/total fatalities/injuries
+# Summarize data over mean/median/max/total fatalities/injuries
 storm_data2 <- storm_data %>%
-        summarize(median_fatalities = median(FATALITIES),
-                  median_injuries = median(INJURIES)) %>%
-        arrange(desc(median_fatalities), desc(median_injuries))
+        summarize(mean_fatalities = mean(FATALITIES),
+                  median_fatalities = median(FATALITIES),
+                  max_fatalities = max(FATALITIES),
+                  total_fatalities = sum(FATALITIES),
+                  mean_injuries = mean(INJURIES),
+                  median_injuries = median(INJURIES),
+                  max_injuries = max(INJURIES),
+                  total_injuries = sum(INJURIES))
+
+# Find most dangerous event type
+most_fatal_median <- storm_data2 %>%
+        arrange(desc(median_fatalities)) %>%
+        head(25)
+
+most_fatal_total <- storm_data2 %>%
+        arrange(desc(total_fatalities)) %>%
+        head(25)
+
+most_injury_median <- storm_data2 %>%
+        arrange(desc(median_injuries)) %>%
+        head(25)
+
+most_injury_total <- storm_data2 %>%
+        arrange(desc(total_injuries)) %>%
+        head(25)
